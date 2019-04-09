3ceeccda20d30228ca0e153c3df0a075182e99b1
Alexander Hare <alexanderhare14@gmail.com>
2019-04-09 17:02:07 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,18 @@
+
+# def reverse_each_word(string)
+#   array = string.split
+#   rev_array = []
+#   array.each do |i|
+#     rev_array << i.reverse
+#   end
+#   rev_array.join(" ")
+# end
+
+
+def reverse_each_word(string)
+  array = string.split
+  rev_array = array.collect do |i|
+    i.reverse
+  end
+  rev_array.join(" ")
+end
